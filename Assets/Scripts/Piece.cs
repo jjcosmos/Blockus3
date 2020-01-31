4708fc20da15ed7d7f5c510e289bc0510f7d6282
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-01-30 19:41:59 -0700

    crappy scripts

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Piece : MonoBehaviour
+{
+    public int pieceID;
+
+    private MeshRenderer rend;   
+    public void OnActivatePiece()
+    {
+        rend.enabled = true;
+        //GetComponent<Animator>().SetBool("Active", true);
+    }
+}
---
+++ Assets/Scripts/Pieces.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pieces : ScriptableObject
+{
+    public List<Piece> usablePieces;
+
+}
