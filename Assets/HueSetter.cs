5f157d722f707ae835e4e06f651b4f50760acc07
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-10 20:59:32 -0700

    multicolored shader

---
+++ Assets/HueSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HueSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        List<Color> colors = new List<Color>{ Color.red, Color.yellow, Color.green, Color.blue };
+        Color c = colors[Random.Range(0,4)];
+        Debug.Log(c.ToString());
+        var block = new MaterialPropertyBlock();
+        block.SetColor("_albedo", c);
+        block.SetColor("_glow", c);
+
+        GetComponent<Renderer>().SetPropertyBlock(block);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
