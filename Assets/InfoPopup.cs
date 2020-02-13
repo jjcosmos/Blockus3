b4b4b82e891c8beabde699039bd0ee0e3cddd7b0
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-12 20:46:08 -0700

    added info button

---
+++ Assets/InfoPopup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPopup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject infoObject;
+    bool showingInfo;
+    [SerializeField] AudioSource source;
+    [SerializeField] AudioClip clip;
+    public void ToggleInfo()
+    {
+        source.PlayOneShot(clip);
+        if (showingInfo)
+        {
+            infoObject.SetActive(false);
+        }
+        else
+        {
+            infoObject.SetActive(true);
+        }
+        showingInfo = !showingInfo;
+    }
+}
